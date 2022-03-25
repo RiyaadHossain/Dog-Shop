baa7169f0aa77f352846c10587aa2365f29725a8
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-25 21:07:00 +0600

    Header Section Done

--- src/index.js
+++ src/index.js
@@ -15,3 +15,16 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/* const arry = [
+  {id:1, name:'JavaScript Book', price: 290, img:''},
+  {id:2, name:'React Book', price: 290, img:''},
+  {id:3, name:'Node Book', price: 290, img:''},
+  {id:4, name:'MERN Book', price: 290, img:''},
+  {id:5, name:'Python Book', price: 290, img:''},
+  {id:6, name:'Web Development Book', price: 290, img:''},
+  {id:7, name:'CSE Book', price: 290, img:''},
+  {id:8, name:'Computer Science Book', price: 290, img:''},
+  {id:9, name:'Programming Book', price: 290, img:''},
+  {id:10, name:'Problem Solving Book', price: 290, img:''}
+] */
