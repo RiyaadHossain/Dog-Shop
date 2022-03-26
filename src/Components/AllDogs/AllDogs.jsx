f6e3f746633a91de963eb4c6a7f3cae01fa5655b
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-26 14:54:51 +0600

    Full Project Complete

--- src/Components/AllDogs/AllDogs.jsx
+++ src/Components/AllDogs/AllDogs.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Dog from "../Dog/Dog";
 import Cart from "../Cart/Cart";
-import './AllDogs.css'
+import "./AllDogs.css";
 
 const AllDogs = () => {
   const [dogs, setDogs] = useState([]);
-  const [cart, setCart] = useState([])
-    
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
@@ -14,18 +14,34 @@ const AllDogs = () => {
   }, []);
 
   const addToCart = (id) => {
-    const newCart = [...cart, id]
-    setCart(newCart)
-  }
+    const newCart = [...cart, id];
+    let selectedDog = []
+    newCart.map((cart) => {
+      if (selectedDog.indexOf(cart) === -1) {
+        selectedDog.push(cart)
+      } else {
+        alert("Sorry, You Can't choose the Same dog Twice. 🙁")
+      }
+    })
+    setCart(selectedDog);
+  };
+  const ChooseOne = () => {
+    const random = Math.floor(Math.random() * cart.length)
+    const choosed = [cart[random]]
+    setCart(choosed)
+  };
+  const ChooseAgain = () => {
+    setCart([])
+  };
   return (
     <div className="main-container">
       <div className="dog-container">
         {dogs.map((dog) => (
-          <Dog key={dog.id} dog={dog} addToCart={addToCart}/>
+          <Dog key={dog.id} dog={dog} addToCart={addToCart} />
         ))}
       </div>
       <div className="liked-dog">
-        <Cart cart={cart}/>
+        <Cart ChooseAgain={ChooseAgain} ChooseOne={ChooseOne} cart={cart} />
       </div>
     </div>
   );
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CartItem from "../CartItem/CartItem";
 import "./Cart.css";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, ChooseAgain, ChooseOne }) => {
   return (
     <div className="cart-container">
       <h2>Cart</h2>
@@ -13,8 +13,8 @@ const Cart = ({ cart }) => {
           ))}
         </div>
         <div className="btn-container">
-          <button className="cart-btn">Choose 1 for Me</button>
-          <button className="cart-btn">Choose Again</button>
+          <button onClick={ChooseOne} className="cart-btn">Choose 1 for Me</button>
+          <button onClick={ChooseAgain} className="cart-btn">Choose Again </button>
         </div>
       </div>
     </div>
